c6b261b74daeb4b52a2c6ee45cad43aa454a2365
Christian Provenzano <18606244+caproven@users.noreply.github.com>
2022-07-24 16:08:03 -0400

    feat: multi-client syncing

--- main.go
+++ main.go
@@ -4,13 +4,47 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
+	"sync"
+	"sync/atomic"
 
 	"github.com/caproven/bytesize"
 	"github.com/gorilla/websocket"
 )
 
 var upgrader = websocket.Upgrader{}
+var pool = connPool{
+	conns: make(map[*websocket.Conn]bool),
+	m:     &sync.Mutex{},
+}
+
+var count uint64 = 0
+
+type connPool struct {
+	conns map[*websocket.Conn]bool // stored as map to simplify removal
+	m     *sync.Mutex
+}
+
+func (p connPool) addClient(c *websocket.Conn) {
+	p.m.Lock()
+	p.conns[c] = true
+	log.Printf("added client: %p\n", c)
+	p.m.Unlock()
+}
+
+func (p connPool) removeClient(c *websocket.Conn) {
+	p.m.Lock()
+	delete(p.conns, c)
+	log.Printf("removed client: %p\n", c)
+	p.m.Unlock()
+}
+
+func (p connPool) broadcast(mt int, data []byte) {
+	p.m.Lock()
+	for c := range p.conns {
+		c.WriteMessage(mt, data)
+	}
+	p.m.Unlock()
+}
 
 func main() {
 	http.HandleFunc("/clicky", func(w http.ResponseWriter, r *http.Request) {
@@ -19,8 +53,13 @@ func main() {
 			log.Printf("upgrade failed: %v", err)
 			return
 		}
-		defer conn.Close()
 		conn.SetReadLimit(3 * bytesize.Kilobyte)
+		defer conn.Close()
+		pool.addClient(conn)
+		defer pool.removeClient(conn)
+
+		// sync client with initial state
+		conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprint(count)))
 
 		for {
 			mt, message, err := conn.ReadMessage()
@@ -32,13 +71,10 @@ func main() {
 			}
 			input := string(message)
 			log.Println("got message:", input)
-			count, _ := strconv.Atoi(input)
 
-			err = conn.WriteMessage(mt, []byte(fmt.Sprint(count+1)))
-			if err != nil {
-				log.Println("write failed:", err)
-				break
-			}
+			atomic.AddUint64(&count, 1)
+
+			pool.broadcast(mt, []byte(fmt.Sprint(count)))
 		}
 	})
 
